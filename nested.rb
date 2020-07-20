c002f1d758f274e9eeaeedbb4301f023662c28a5
Ian Mayton <ian.mayton@gmail.com>
2020-07-20 19:20:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ programmer_hash[:yukihiro_matsumoto] = {
    :languages => ["LISP", "C"]
    }
    
-  return programmer_hash[:yukihiro_matsumoto] 
+  programmer_hash
 end
 
 def changing_alan
@@ -110,7 +110,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
    alans_new_info = "GUI"
    programmer_hash[:alan_kay][:known_for] = alans_new_info
-   programmer_hash[:alan_kay][:known_for]
+   programmer_hash
 end
 
 def adding_to_dennis
@@ -132,5 +132,5 @@ def adding_to_dennis
   array = programmer_hash[:dennis_ritchie][:languages] 
   array = array << "Assembly"
   programmer_hash[:dennis_ritchie][:languages] = array
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash
 end
