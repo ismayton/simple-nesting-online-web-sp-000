a43ae6644786d2821be69ab985364d0168ea936e
Ian Mayton <ian.mayton@gmail.com>
2020-07-20 19:14:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,10 +66,6 @@ end
 
 def adding_matz
 
-programmer_hash[:yukihiro_matsumoto] = {
-  :known_for => "Ruby",
-   :languages => ["LISP", "C"]
-   }
 
 	programmer_hash = 
  		{
@@ -87,6 +83,11 @@ programmer_hash[:yukihiro_matsumoto] = {
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+   }
+   
   programmer_hash[:yukihiro_matsumoto] 
 end
 
