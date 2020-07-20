5b0548c052baf8c58da4e09f37a895ad31da8513
Ian Mayton <ian.mayton@gmail.com>
2020-07-20 19:21:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  array = programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  programmer_hash[:dennis_ritchie][:languages] = array
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 end
